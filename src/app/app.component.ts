c7be80450280bd0c219b8c5e49609f950a000a39
adel <adelabodoma@gmail.com>
2020-03-15 19:46:33 +0200

    add fuctions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+import * as data from '../assets/data.json'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'page-layout';
+  private list: List
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.list = data.default['page-info'];
+
+    setTimeout(() => {
+      // this.addRow('2-1')
+      // this.addRow('3')
+      // this.addRow('1-2')
+      // this.addRow('2')
+      // this.addRow('1')
+    }, 2000);
+  }
+
+  addRow(type, index) {
+    let columns;
+
+
+    switch (type) {
+      case '1-2':
+        columns = Array(2).fill({ id: 10, components: [] });
+        break;
+      case '2-1':
+        columns = Array(2).fill({ id: 10, components: [] });
+        break;
+      default:
+        columns = Array(+type).fill({ id: 10, components: [] });
+        break;
+    }
+
+
+
+    this.list.rows.splice(index, 0, {
+      id: '20',
+      type: type,
+      columns: columns
+    })
+ 
+    console.log(this.list);
+
+  }
+
+}
+
+
+
+
+interface List {
+  rows: ListItem[]
+}
+
+interface ListItem {
+  id: string;
+  type: string;
+  columns: ItemComponents[]
+}
+interface ItemComponents {
+  id: string;
+  components: ItemComponent[]
+}
+interface ItemComponent {
+  id: string;
+  title: string;
+  URL: string;
+  icon: string
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NgbDropdown],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
